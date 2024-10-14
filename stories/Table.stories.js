68146d3aac54c85cf7b922e4974fe9ee241554a5
Julien Déramond <juderamond@gmail.com>
2024-10-14 12:20:50 +0200

    feat: add basic table story

---
+++ stories/Table.stories.js
@@ -0,0 +1,11 @@
+import { createTable } from './Table';
+
+export default {
+  title: 'Example/Table',
+  render: () => createTable(),
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Table = {};
