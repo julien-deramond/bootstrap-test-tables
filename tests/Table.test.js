e3a66367dc5cf029a1c52b3c3cfbec927d3caf3d
Julien Déramond <juderamond@gmail.com>
2024-10-14 16:50:39 +0200

    feat: reduce the amount of snapshots

--- tests/Table.test.js
+++ tests/Table.test.js
@@ -3,8 +3,8 @@ import { sizes, tableColors, trTableColors } from '../stories/Table.const';
 
 test.describe('Table Component', () => {
   for (const size of sizes) {
-    for (const tableColor of tableColors) {
-      for (const trTableColor of trTableColors) {
+    for (const tableColor of /* tableColors */ ['default', 'primary']) {
+      for (const trTableColor of /* trTableColors*/ ['default', 'secondary']) {
         test(`size: ${size}, tableColor: ${tableColor}, trTableColor: ${trTableColor}`, async ({ page }) => {
           await page.goto(`http://localhost:6006/iframe.html?id=example-table--table&args=size:${size};tableColor:${tableColor};trTableColor:${trTableColor}`);
           const table = await page.locator('.storybook-table');
